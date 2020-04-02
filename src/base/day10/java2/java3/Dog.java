e53853f088564278dc15924223124df78825b741
guyuqiang <1243952362@qq.com>
2020-04-02 23:59:16 +0800

    day16

---
+++ src/base/day10/java2/java3/Dog.java
@@ -0,0 +1,5 @@
+package base.day10.java2.java3;
+
+public class Dog {
+
+}
---
+++ src/base/day14/java/Man.java
@@ -0,0 +1,23 @@
+package base.day14.java;
+
+public class Man extends Person{
+	
+	public Man(String name, int age) {
+		super(name, age);
+	}
+
+	public void earnMoney(){
+		System.out.println("男人负责挣钱养家");
+	}
+	
+	@Override
+	public void eat(){
+		System.out.println("男人多吃肉，长肌肉");
+	}
+	
+	@Override
+	public void walk(){
+		System.out.println("男人霸气的走路");
+	}
+
+}
